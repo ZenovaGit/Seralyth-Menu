f093eb3ea06a651c9028918bcf0aae4965432244
kingofnetflix <72701796+kingofnetflix@users.noreply.github.com>
2026-02-24 22:59:47 -0600

    We're going back to 1.0.0

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -27,7 +27,7 @@ namespace Seralyth
         public const string Name = "Seralyth Menu";
         public const string Description = "Created by kingofnetflix";
         public const string BuildTimestamp = "2026-02-23T03:36:03Z";
-        public const string Version = "8.3.0";
+        public const string Version = "1.0.0";
 
         public const string BaseDirectory = "SeralythMenu";
         public const string ClientResourcePath = "Seralyth.Resources.Client";
